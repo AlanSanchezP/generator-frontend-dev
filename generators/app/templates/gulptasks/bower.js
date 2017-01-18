9d327783aff38ba21e84edf61d52dca2b770b709
Alan Sanchez <sanchezpineda03@gmail.com>
2017-01-18 14:28:03 -0600

    Created build:bower template file.

---
+++ generators/app/templates/gulptasks/bower.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+  config = require('../gulpconfig'),
+  bowerFiles = require('main-bower-files'),
+  cssmin = require('gulp-minify-css'),
+  cssconcat = require('gulp-concat-css'),
+  jsconcat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  gulpIf = require('gulp-if'),
+  argv = require('yargs').argv,
+  production = argv.production;
+
+gulp.task('build:bower', function () {
+  var baseDir = production ? config.paths.dist : config.paths.build;
+
+  gulp.src(bowerFiles('**/*.css'))
+    .pipe(cssconcat(config.outputs.libs('css')))
+    .pipe(gulpIf(production, cssmin({processImport: false})))
+    .pipe(gulp.dest(baseDir.css));
+
+  gulp.src(bowerFiles('**/*.js'))
+    .pipe(jsconcat(config.outputs.libs('js')))
+    .pipe(gulpIf(production, uglify()))
+    .pipe(gulp.dest(baseDir.js));
+});
