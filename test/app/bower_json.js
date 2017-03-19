da08df26ffc4e99d6ec8ecbf4cab5123d28a8d37
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:02:20 -0600

    Tested bower.json: Foundation and FontAwesome.

--- test/app/bower_json.js
+++ test/app/bower_json.js
@@ -89,3 +89,30 @@ describe('generator-frontend-dev:app', function () {
     assert.noFileContent('bower.json', 'font-awesome');
   });
 });
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        name: PROJECT_NAME,
+        onlyFrontend: true,
+        cssFramework: 'Foundation',
+        useFontAwesome: true
+      })
+      .toPromise();
+  });
+
+  it('Creates bower.json using Foundation and FontAwesome', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/foundation_fa.json'),
+      JSONContent = null;
+
+    try {
+      JSONContent = JSON.parse(fs.readFileSync(filepath, { encoding: 'UTF-8' }));
+    } catch(e) {
+      JSONContent = null;
+    }
+
+    assert.file(['bower.json']);
+    assert.jsonFileContent('bower.json', JSONContent);
+  });
+});
