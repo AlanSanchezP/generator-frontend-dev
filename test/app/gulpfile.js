f8938851b1cb4917ef8915c1085daf23ec437d01
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:45:36 -0600

    Tested gulpfile: Only-frontend projects.

---
+++ test/app/gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+var path = require('path'),
+  assert = require('yeoman-assert'),
+  helpers = require('yeoman-test'),
+  fs = require('fs'),
+  APP_PATH = '../../generators/app',
+  TEMPLATES_PATH = APP_PATH + '/test_templates/gulpfile_js';
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        onlyFrontend: true
+      })
+      .toPromise();
+  });
+
+  it('Creates gulpfile for only-frontend projects', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/only_frontend.js'),
+      content = fs.readFileSync(filepath, { encoding: 'UTF-8' });
+
+    assert.file('gulpfile.js');
+    assert.fileContent('gulpfile.js', content);
+  });
+});
