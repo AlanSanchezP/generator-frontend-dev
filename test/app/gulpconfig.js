edf8f8d7935bea096958b75ad847478812a9edca
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:51:26 -0600

    Tested gulpconfig file: Only-frontend projects.

---
+++ test/app/gulpconfig.js
@@ -0,0 +1,27 @@
+'use strict';
+var path = require('path'),
+  assert = require('yeoman-assert'),
+  helpers = require('yeoman-test'),
+  fs = require('fs'),
+  APP_PATH = '../../generators/app',
+  TEMPLATES_PATH = APP_PATH + '/test_templates/gulpconfig_js',
+  PROJECT_NAME = 'Test project';
+
+  describe('generator-frontend-dev:app', function () {
+    before(function () {
+      return helpers.run(path.join(__dirname, APP_PATH))
+        .withPrompts({
+          name: PROJECT_NAME,
+          onlyFrontend: true
+        })
+        .toPromise();
+    });
+
+    it('Creates gulpfile for only-frontend projects', function () {
+      var filepath = path.join(__dirname, TEMPLATES_PATH + '/only_frontend.js'),
+        content = fs.readFileSync(filepath, { encoding: 'UTF-8' });
+
+      assert.file('gulpconfig.js');
+      assert.fileContent('gulpconfig.js', content);
+    });
+  });
