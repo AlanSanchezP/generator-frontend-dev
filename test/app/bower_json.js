198467b4d918efdbb6f2f216d015b293538be206
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:13:43 -0600

    Tested bower.json: Uses FontAwesome without framework.

--- test/app/bower_json.js
+++ test/app/bower_json.js
@@ -199,3 +199,30 @@ describe('generator-frontend-dev:app', function () {
     assert.noFileContent('bower.json', 'font-awesome');
   });
 });
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        name: PROJECT_NAME,
+        onlyFrontend: true,
+        cssFramework: 'Other',
+        useFontAwesome: true
+      })
+      .toPromise();
+  });
+
+  it('Creates bower.json without framework but using FontAwesome', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/other_fa.json'),
+      JSONContent = null;
+
+    try {
+      JSONContent = JSON.parse(fs.readFileSync(filepath, { encoding: 'UTF-8' }));
+    } catch(e) {
+      JSONContent = null;
+    }
+
+    assert.file(['bower.json']);
+    assert.jsonFileContent('bower.json', JSONContent);
+  });
+});
