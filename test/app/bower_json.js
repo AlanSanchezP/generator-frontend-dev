217b470294b0bb020aad70178f976bc018c4f974
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 20:55:12 -0600

    Tested bower.json: Bootstrap and FontAwesome.

--- test/app/bower_json.js
+++ test/app/bower_json.js
@@ -34,3 +34,30 @@ describe('generator-frontend-dev:app', function () {
     assert.noFileContent('bower.json', 'font-awesome');
   });
 });
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        name: PROJECT_NAME,
+        onlyFrontend: true,
+        cssFramework: 'Bootstrap',
+        useFontAwesome: true
+      })
+      .toPromise();
+  });
+
+  it('Creates bower.json using Bootstrap and FontAwesome', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/bootstrap_fa.json'),
+      JSONContent = null;
+
+    try {
+      JSONContent = JSON.parse(fs.readFileSync(filepath, { encoding: 'UTF-8' }));
+    } catch(e) {
+      JSONContent = null;
+    }
+
+    assert.file(['bower.json']);
+    assert.jsonFileContent('bower.json', JSONContent);
+  });
+});
