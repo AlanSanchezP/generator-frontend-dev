ad5fdcc7e26954f4f5f44e49419dcc62e8b3f987
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:07:44 -0600

    Tested bower.json: Pure.css and FontAwesome.

--- test/app/bower_json.js
+++ test/app/bower_json.js
@@ -144,3 +144,30 @@ describe('generator-frontend-dev:app', function () {
     assert.noFileContent('bower.json', 'font-awesome');
   });
 });
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        name: PROJECT_NAME,
+        onlyFrontend: true,
+        cssFramework: 'Pure.css',
+        useFontAwesome: true
+      })
+      .toPromise();
+  });
+
+  it('Creates bower.json using Pure.css and FontAwesome', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/pure_fa.json'),
+      JSONContent = null;
+
+    try {
+      JSONContent = JSON.parse(fs.readFileSync(filepath, { encoding: 'UTF-8' }));
+    } catch(e) {
+      JSONContent = null;
+    }
+
+    assert.file(['bower.json']);
+    assert.jsonFileContent('bower.json', JSONContent);
+  });
+});
