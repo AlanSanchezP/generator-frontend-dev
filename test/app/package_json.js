a8bac892eb8695af2894b8eb2a5baf4c8f75eaf8
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:25:34 -0600

    Tested package.json: Not only-frontend projects.

--- test/app/package_json.js
+++ test/app/package_json.js
@@ -27,9 +27,38 @@ describe('generator-frontend-dev:app', function () {
       JSONContent = null;
     }
 
-    console.log(JSONContent);
+    assert.file(['package.json']);
+    assert.jsonFileContent('package.json', JSONContent);
+  });
+});
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        name: PROJECT_NAME,
+        onlyFrontend: false
+      })
+      .toPromise();
+  });
+
+  it('Creates package.json for not only-frontend projects', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/not_only_frontend.json'),
+      JSONContent = null;
+
+    try {
+      JSONContent = JSON.parse(fs.readFileSync(filepath, { encoding: 'UTF-8' }));
+    } catch(e) {
+      JSONContent = null;
+    }
 
     assert.file(['package.json']);
     assert.jsonFileContent('package.json', JSONContent);
+    assert.noFileContent('package.json', 'nunjucks');
+    assert.noFileContent('package.json', 'gulp-gh-pages');
+    assert.noFileContent('package.json', 'gulp-data');
+    assert.noFileContent('package.json', 'gulp-nunjucks-render');
+    assert.noFileContent('package.json', 'portfinder');
+    assert.noFileContent('package.json', 'gulp-connect');
   });
 });
