76cadc9ca7849bb36bea6623d6a79f877017ea94
Alan Sanchez <sanchezpineda03@gmail.com>
2017-03-18 21:35:52 -0600

    Tested .gitignore: Only-frontend projects.

---
+++ test/app/gitignore.js
@@ -0,0 +1,27 @@
+'use strict';
+var path = require('path'),
+  assert = require('yeoman-assert'),
+  helpers = require('yeoman-test'),
+  fs = require('fs'),
+  APP_PATH = '../../generators/app',
+  TEMPLATES_PATH = APP_PATH + '/test_templates/gitignore',
+  PROJECT_NAME = 'Test project';
+
+describe('generator-frontend-dev:app', function () {
+  before(function () {
+    return helpers.run(path.join(__dirname, APP_PATH))
+      .withPrompts({
+        name: PROJECT_NAME,
+        onlyFrontend: true
+      })
+      .toPromise();
+  });
+
+  it('Creates .gitignore for only-frontend projects', function () {
+    var filepath = path.join(__dirname, TEMPLATES_PATH + '/only_frontend.txt'),
+      content = fs.readFileSync(filepath, { encoding: 'UTF-8' });
+
+    assert.file('.gitignore');
+    assert.fileContent('.gitignore', content);
+  });
+});
